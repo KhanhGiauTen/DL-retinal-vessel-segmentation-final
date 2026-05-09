4cf0c07f34c32bb5e1410af15926304fa6850bd6
Dong <duongquangdong1008@gml.com>
2026-05-10 01:49:55 +0700

    Done preprocess + dataset

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,4 +1,8 @@
 import numpy as np
+import os
+import tqdm
+import torch
+EXPORT_BASE = "../Drive_processed_dataset"
 
 def normalize_mask(mask, **kwargs):
     return mask.astype(np.float32) / 255.0
@@ -12,3 +16,26 @@ def tensor_to_image(tensor):
     
     img = np.clip(img, 0, 1)
     return img
+
+def export_dataset_to_disk(dataset, split_name):
+    print(f"Exporting {split_name} ({len(dataset)} samples)...")
+    
+    split_dir = os.path.join(EXPORT_BASE, split_name)
+    os.makedirs(split_dir, exist_ok=True)
+    
+    for i in tqdm(range(len(dataset))):
+        sample = dataset[i]
+
+        save_dict = {
+            "image": sample[0],
+            "mask": sample[1],
+            "manual": sample[2]
+        }
+        
+        # tập Test
+        if len(sample) == 4:
+            save_dict["manual_2"] = sample[3]
+            
+        # Lưu file dưới dạng .pt (PyTorch Data)
+        save_path = os.path.join(split_dir, f"sample_{i}.pt")
+        torch.save(save_dict, save_path)
